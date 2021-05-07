2d94ba291faa0af1111723d11cb1e4ecc81ed321
Masud Hussain <mhussain790@gmail.com>
2021-05-06 19:33:56 -0700

    Initial commit of the ListMax program files.

---
+++ list_max.py
@@ -0,0 +1,40 @@
+"""
+Name: Masud Hussain
+Course: CS162
+Assignment: 6A
+"""
+
+"""
+Write a recursive function named list_max that takes as its parameter 
+a list of numbers and returns the maximum value in the list. 
+You can assume the list contains at least one element. 
+If multiple elements of the list are tied for the maximum, 
+you would still return that value.
+
+You cannot call the built-in max() function.
+
+You may use default arguments and/or helper functions.
+
+Your recursive function must not:
+
+use any loops
+use any variables declared outside of the function
+use any mutable default arguments
+The file must be named: list_max.py
+"""
+
+
+def list_max(a_list):
+
+    if len(a_list) == 1:
+        return a_list[0]
+
+    if a_list[0] > a_list[1]:
+        a_list.pop(1)
+    else:
+        a_list.pop(0)
+    return list_max(a_list)
+
+
+temps = [5, 8, 3, 2]
+list_max(temps)
